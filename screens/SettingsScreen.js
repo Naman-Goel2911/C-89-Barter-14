bfac72a9129c66c21119c933e8d17c5eb67c2faf
Naman Goel <vakulg@gmail.com>
2021-06-01 17:07:51 +0530

    Added Settings screen

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,8 +1,57 @@
 import * as React from 'react'
-import {Text, View} from 'react-native'
+import {Text, View, TextInput, TouchableOpacity, StyleSheet} from 'react-native'
 import {Header} from 'react-native-elements'
+import db from '../config'
+import firebase from 'firebase'
 
 export default class SettingsScreen extends React.Component{
+    constructor()
+    {
+        super()
+        this.state = {
+            firstName: '',
+            lastName: '',
+            address: '',
+            contact: '',
+            emailId: '',
+            docId: ''
+        }
+    }
+
+    componentDidMount = ()=> {
+        this.getUserDetails()
+    }
+
+    getUserDetails = () => {
+        var user = firebase.auth().currentUser;
+        var email = user.email
+        db.collection('users').where('email_id', '==', email).get()
+        .then((snapshot)=> {
+            snapshot.forEach((doc)=> {
+                var data = doc.data()
+                this.setState({
+                    emailId: data.email_id,
+                    firstName: data.first_name,
+                    lastName: data.last_name,
+                    address: data.address,
+                    contact: data.contact,
+                    docId: doc.id
+                })
+            })
+        })
+    }
+
+    updateUserDetails = () => {
+        db.collection('users').doc(this.state.docId).update({
+            first_name: this.state.firstName,
+            last_name: this.state.lastName,
+            address: this.state.address,
+            contact: this.state.contact
+        })
+        
+        alert('Profile updated successfully')
+    }
+
     render()
     {
         return(
@@ -11,12 +60,74 @@ export default class SettingsScreen extends React.Component{
                 backgroundColor = {'#9c8210'}
                 centerComponent = 
                 {{
-                    text: 'Barter',
+                    text: 'Settings',
                     style: { color: '#fff', fontSize: 20 },
                 }}
                 />
-                <Text>Settings Screen</Text>
+                <View>
+                    <TextInput 
+                    style = {styles.formTextInput}
+                    placeholder = 'First Name'
+                    maxLength = {8}
+                    onChangeText = {(text)=> {
+                        this.setState({
+                            firstName: text
+                        })
+                    }}
+                    value = {this.state.firstName}
+                    />
+                    <TextInput 
+                    style = {styles.formTextInput}
+                    placeholder = 'Last Name'
+                    maxLength = {8}
+                    onChangeText = {(text)=> {
+                        this.setState({
+                            lastName: text
+                        })
+                    }}
+                    value = {this.state.lastName}
+                    />
+                    <TextInput 
+                    style = {styles.formTextInput}
+                    placeholder = 'Contact'
+                    maxLength = {10}
+                    keyboardType = 'numeric'
+                    onChangeText = {(text)=> {
+                        this.setState({
+                            contact: text
+                        })
+                    }}
+                    value = {this.state.contact}
+                    />
+                    <TextInput 
+                    style = {styles.formTextInput}
+                    placeholder = 'Address'
+                    multiline = {true}
+                    onChangeText = {(text)=> {
+                        this.setState({
+                            address: text
+                        })
+                    }}
+                    value = {this.state.address}
+                    />
+                    <TouchableOpacity
+                    style = {styles.button}
+                    onPress = {()=> {
+                        this.updateUserDetails()
+                    }}
+                    >
+                        <Text style = {styles.buttonText}>Save</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({ 
+    container : { flex:1,  justifyContent: 'center'}, 
+    formContainer:{ flex:1, width:'100%', alignItems: 'center' }, 
+    formTextInput:{ width:"75%", height:35, alignSelf:'center', borderColor:'#ffab91', borderRadius:10, borderWidth:1, marginTop:20, padding:10, }, 
+    button:{ width:"75%", height:50, justifyContent:'center', alignItems:'center', borderRadius:10, alignSelf: 'center', backgroundColor:"#ff5722", shadowColor: "#000", shadowOffset: { width: 0, height: 8, }, shadowOpacity: 0.44, shadowRadius: 10.32, elevation: 16, marginTop:20 }, 
+    buttonText:{ fontSize:25, fontWeight:"bold", color:"#fff" } 
+})
